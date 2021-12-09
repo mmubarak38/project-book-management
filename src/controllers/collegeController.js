96bf544212187a177996679ddac9a9d5e858efdd
mmubarak38 <mmubara38@gmail.com>
2021-12-09 08:29:33 +0530

    fianl submission

--- src/controllers/collegeController.js
+++ src/controllers/collegeController.js
@@ -12,58 +12,74 @@ const isValidRequestBody = function (requestBody) {
     return Object.keys(requestBody).length > 0
 }
 
-const ValidURL = function (userInput) {
-    var res = userInput.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%.\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%\+.~#?&//=]*)/g);
-    if (res == null)
-        return false;
-    else
-        return true;
+// const ValidURL = function (userInput) {
+//     var res = userInput.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+//     if (res == null)
+//         return false;
+//     else
+//         return true;
+// }
+const isImgLink= function (url) {
+    if(typeof url !== 'string') return false;
+    return(url.match(/^http[^\?]*.(jpg|jpeg|gif|png|tiff|bmp)(\?(.*))?$/gmi) != null);
 }
+
+
 
 const createCollege = async function (req, res) {
     try {
+        // Validation starts
         const requestBody = req.body;
         if (!isValidRequestBody(requestBody)) {
             res.status(400).send({ status: false, message: 'Invalid request parameters. Please provide college details' })
             return
         }
+            let  { name, fullName, logoLink, isDeleted } = requestBody; // Object destructing
 
-        // Extract params
-        const { name, fullName, logoLink, isDeleted } = requestBody; // Object destructing (it will devide the body request for access indivisually)
 
-        // Validation starts
         if (!isValid(name)) {
             res.status(400).send({ status: false, message: 'name is required' })
             return
         }
+        name = name.trim();
+
+        let isNameAlreadyUsed = await collegeModel.findOne({name}); 
+
+        if(isNameAlreadyUsed) {
+            res.status(400).send({status: false, message: `${name} is already registered`})
+            return
+        }
+
         if (!isValid(fullName)) {
             res.status(400).send({ status: false, message: 'fullName is required' })
             return
         }
-        if (!isValid(logoLink)){
-            res.status(400).send({ status: false, message: ' logo is required' })
-        return
-            
-        }
-        if (!ValidURL(logoLink)) {
-            res.status(400).send({ status: false, message: 'Invalid logo input' })
+
+        if (!isValid(logoLink)) {
+            res.status(400).send({ status: false, message: 'logolink is required' })
             return
         }
-       
-
-
-
-        // Validation ends
-
+        if (!isImgLink(logoLink)) {
+            res.status(400).send({ status: false, message: 'invalid logolink' })
+            return
+        }
+       // console.log(ValidURL(logoLink))
+    //    if (isDeleted) {  ///(isDeleted = true) or ==  or ===
+    //     res.status(400).send({ status: false, message: "Cannot input isDeleted as true while registering" })
+    //     return
+    //      }
+        // Validation end
 
         const Data = { name, fullName, logoLink, isDeleted }
         let savedData = await collegeModel.create(Data)
+        console.log(savedData)
         res.status(201).send({ status: true, data: savedData })
     } catch (error) {
-        res.status(500).send({ status: false, msg: error.message })
+        res.status(500).send({ status: false, msg: error })//specifies which error 
+        console.log(error)
     }
 }
-
+module.exports.createCollege = createCollege;
 
 
 /*
@@ -79,35 +95,32 @@ The response structure should look like this
 const getAllIntern = async function (req, res) {
     try {
 
-        let collegeName = req.query.collegeName;
 
+
+        let collegeName = req.query.collegeName;
+        
         if (!collegeName) {
-            res.status(400).send({ status: false, msg: 'put CollegeName' })
+           return res.status(400).send({ status: false, msg: 'put CollegeName' })
         }
 
-        let collegeDetail = await collegeModel.findOne({ name: collegeName })
+        let collegeDetail = await collegeModel.findOne({ name: collegeName , isDeleted: false })
 
-        if (!collegeDetail) {
-            res.status(400).send({ status: false, msg: 'college name not present' })
+        if (!collegeDetail)
+             {
+            return res.status(400).send({ status: false, msg: 'college name not present' })//ask to mentor
         }
 
         console.log(collegeDetail._id)
 
         let internDetail = await internModel.find({ collegeId: collegeDetail._id, isDeleted: false }).select({ _id: 1, name: 1, email: 1, mobile: 1 })
-        
         console.log(internDetail)
 
-
-
-
-        let result = {
+      let result = {
             name: collegeDetail.name,
             FullName: collegeDetail.fullName,
             LogoLink: collegeDetail.logoLink,
 
         }
-
-
         // if (internDetail.length === 0) {
         //     return res.status(201).send({ status: true, result, msg: 'intern Details not present' })
         // }
@@ -118,17 +131,14 @@ const getAllIntern = async function (req, res) {
             LogoLink: collegeDetail.logoLink,
             interests: internDetail
         }
-
-
-        // if (!result2) {
+         // if (!result2) {
         //     res.status(400).send({ status: false, msg: 'data is not present' })
         // }
-       
-        
+         
         if (internDetail.length === 0) {
             return res.status(201).send({ status: true, result, msg: 'intern Details not present' })
         }else{
-            res.status(201).send({status: true, data: result2})
+            res.status(200).send({status: true, data: result2})
         }
 
         
@@ -137,7 +147,10 @@ const getAllIntern = async function (req, res) {
     }
 }
 
-
-
-module.exports.createCollege = createCollege;
 module.exports.getAllIntern = getAllIntern;
+
+
+
+// console.log(isImgLink('https://example.com/img.jpg')); // true
+// console.log(isImgLink('https://example.com/any-route?param=not-img-file.jpg')); // false
+// console.log(isImgLink('https://example.com/img.jpg?param=123')); // true
